e25a3a63b19645d716b9e72abdcd8833369f2052
Tiffani <tshilts2@pdx.edu>
2021-01-30 21:06:47 -0800

    fully functioning code

---
+++ BasicCalc/app/src/main/java/com/bignerdranch/android/basiccalc/MainActivity.java
@@ -0,0 +1,203 @@
+package com.bignerdranch.android.basiccalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "CalculatorActivity";
+
+    private EditText minput1;
+    private EditText minput2;
+
+    private TextView moutput;
+
+    private Button mCalcAddButton;
+    private Button mCalcSubButton;
+    private Button mCalcMultiButton;
+    private Button mCalcDivButton;
+    private Button mCalcPercentButton;
+    private Button mCalcSqrtButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        minput1 = (EditText) findViewById(R.id.editText_op1);
+        minput2 = (EditText) findViewById(R.id.editText_op2);
+
+        moutput = (TextView) findViewById(R.id.textView_result);
+
+        // implement addition function
+        mCalcAddButton = (Button) findViewById(R.id.button_op_plus);
+        mCalcAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, true)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter two positive decimal numbers.",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Double.valueOf(minput1.getText().toString()) +
+                            Double.valueOf(minput2.getText().toString());
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+        // implement subtraction function
+        mCalcSubButton = (Button) findViewById(R.id.button_op_minus);
+        mCalcSubButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, true)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter two positive decimal numbers.",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Double.valueOf(minput1.getText().toString()) -
+                            Double.valueOf(minput2.getText().toString());
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+        // implement multiplication function
+        mCalcMultiButton = (Button) findViewById(R.id.button_op_multi);
+        mCalcMultiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, true)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter two positive decimal numbers.",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Double.valueOf(minput1.getText().toString()) *
+                            Double.valueOf(minput2.getText().toString());
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+        // implement divide function
+        mCalcDivButton = (Button) findViewById(R.id.button_op_div);
+        mCalcDivButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, true) || (Double.valueOf(minput2.getText().toString()) == 0)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter two positive decimal numbers." +
+                                    "You may not divide by zero.",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Double.valueOf(minput1.getText().toString()) /
+                            Double.valueOf(minput2.getText().toString());
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+        // implement percent function
+        mCalcPercentButton = (Button) findViewById(R.id.button_op_pct);
+        mCalcPercentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, false)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter a positive number for operand one",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Double.valueOf(minput1.getText().toString()) / 100.0;
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+        // implement square root function
+        mCalcSqrtButton = (Button) findViewById(R.id.button_op_sqrt);
+        mCalcSqrtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isValidInput(true, false)) {
+                    Toast.makeText(MainActivity.this,
+                            "Please enter a positive number for operand one",
+                            Toast.LENGTH_SHORT).show();
+
+                    clear_input();
+
+                    minput1.requestFocus();
+                } else {
+                    double result = Math.sqrt(Double.valueOf(minput1.getText().toString()));
+                    moutput.setText(String.format("%.03f", result));
+                }
+            }
+        });
+
+    }
+
+    private boolean isValidInput(boolean NeedI1, boolean NeedI2)
+    {
+        String inputOneString = minput1.getText().toString();
+        String inputTwoString = minput2.getText().toString();
+
+        if (NeedI1)
+        {
+            if (inputOneString.length() == 0)
+            {
+                return false;
+            }
+            else if (Double.parseDouble(inputOneString) < 0.00)
+            {
+                return false;
+            }
+        }
+
+        if (NeedI2)
+        {
+            if (inputTwoString.length() == 0)
+            {
+                return false;
+            }
+            else if (Double.parseDouble(inputTwoString) < 0.00)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void clear_input()
+    {
+        minput1.setText("");
+        minput2.setText("");
+        moutput.setText("");
+    }
+}
+
